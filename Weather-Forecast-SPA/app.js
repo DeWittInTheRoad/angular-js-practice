7f1c684ee7d7cd6b11471fea1276d55dc70a9390
Brandon <DeWittInTheRoad@gmail.com>
2019-05-25 12:00:34 -0400

    Service Finished

--- Weather-Forecast-SPA/app.js
+++ Weather-Forecast-SPA/app.js
@@ -12,19 +12,33 @@ weatherApp.config(function ($routeProvider){
 			controller: 'homeController'
 		})
 
-		.when('forecast', {
+		.when('/forecast', {
 			templateUrl: 'pages/forecast.htm',
 			controller: 'forecastController'
 		})
 
 });
 
-	weatherApp.controller('homeController', ['$scope',
-		function($scope) {
+//SERVICES
+
+weatherApp.service('cityService', function(){
+    this.city = "New York, NY";
+});
+
+	weatherApp.controller('homeController', ['$scope', 'cityService',
+		function($scope, cityService) {
+
+	    $scope.city = cityService.city;
+
+	    $scope.$watch('city', function() {
+	        cityService.city = $scope.city;
+      });
 
 		}]);
 
-	weatherApp.controller('forecastController', ['$scope',
-		function($scope) {
+	weatherApp.controller('forecastController', ['$scope', 'cityService',
+		function($scope, cityService) {
+
+        $scope.city = cityService.city;
 
 		}]);
