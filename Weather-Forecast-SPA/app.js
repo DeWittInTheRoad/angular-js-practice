acc04cfa30209d9188e3041d135a31a91129e2e1
Brandon <DeWittInTheRoad@gmail.com>
2019-05-25 14:00:49 -0400

    API Call added

--- Weather-Forecast-SPA/app.js
+++ Weather-Forecast-SPA/app.js
@@ -22,7 +22,7 @@ weatherApp.config(function ($routeProvider){
 //SERVICES
 
 weatherApp.service('cityService', function(){
-    this.city = "New York, NY";
+    this.city = "New York";
 });
 
 	weatherApp.controller('homeController', ['$scope', 'cityService',
@@ -36,9 +36,24 @@ weatherApp.service('cityService', function(){
 
 		}]);
 
-	weatherApp.controller('forecastController', ['$scope', 'cityService',
-		function($scope, cityService) {
+	weatherApp.controller('forecastController', ['$scope', '$resource', 'cityService',
+		function($scope, $resource, cityService) {
 
         $scope.city = cityService.city;
 
+        $scope.weatherAPI =
+	        $resource("http://api.openweathermap.org/data/2.5/weather", {
+	        	callback: "JSON_CALLBACK"}, {get: {method: "JSONP"}});
+
+        $scope.apiIdKey="49287c18b4037403164d223c186bec2f";
+
+        $scope.weatherResult = $scope.weatherAPI.get({q: $scope.city, APPID: $scope.apiIdKey });
+
+        $scope.convertToFahrenheit = (kelvin) =>
+        	Math.round((1.8 * (kelvin - 273)) + 32)
+
+				$scope.convertToDate = (dt) =>
+					new Date(dt * 1000)
+
 		}]);
+
